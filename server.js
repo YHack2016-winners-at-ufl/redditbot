526ec6fe9d4d4e60f5d1ae1588a5938a82b27cfe
Joshua Benner <bennerster@gmail.com>
2016-11-12 09:00:38 -0500

    added scraping reddit and comments

---
+++ server.js
@@ -0,0 +1,79 @@
+/**
+ * Created by joshbenner on 11/12/16.
+ */
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+
+    //All the web scraping magic will happen here
+
+    url = 'https://www.reddit.com/r/RoastMe/';
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            $('.comments').filter(function(){
+
+                // Let's store the data we filter into a variable so we can easily see what's going on.
+
+                var data = $(this);
+
+
+                // In examining the DOM we notice that the title rests within the first child element of the header tag.
+                // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+
+                //roast = data.text();
+                urls = data[0].attribs.href;
+                console.log(urls);
+
+
+                request(urls, function(error, response, html){
+
+                    // First we'll check to make sure no errors occurred when making the request
+
+                    if(!error) {
+                        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+                        $ = cheerio.load( html );
+
+                        $('.md').filter(function() {
+                            data = $(this);
+                            console.log(data.children().first().text());
+                            //comment = data[0].children().first().text();
+                            //console.log(comment);
+                        });
+
+
+
+                    }
+                });
+
+                // Once we have our title, we'll store it to the our json object.
+
+            });
+            // Finally, we'll define the variables we're going to capture
+
+            var roast;
+            var json = { roast : ""};
+        }
+    });
+});
+
+app.listen('8081');
+
+console.log('Magic happens on port 8081');
+
+exports = module.exports = app;
