6d28e248b7ae2b7985708346a242db8e6b6d42de
Joshua Benner <bennerster@gmail.com>
2016-11-12 20:35:35 -0500

    Added arrays and only scrapes onstart

--- server.js
+++ server.js
@@ -6,16 +6,23 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var bot = require('./redditbot');
+var _ = require('underscore');
 var app     = express();
 
+function Roast(message,votes){
+    this.msg = message || "";
+    this.upvotes = votes || 0;
+}
+
 var callback = function(roast){
-  //do something with roast
+    //do something with roast
     bot._onMessage(roast);
 };
 
 var scraper = {
     counter: 0,
     url: 'https://www.reddit.com/r/RoastMe/',
+    roasts: [],
     scrapeReddit: function(callback) {
         var that = this;
         //All the web scraping magic will happen here
@@ -23,7 +30,6 @@ var scraper = {
         // The structure of our request call
         // The first parameter is our URL
         // The callback function takes 3 parameters, an error, response status code and the html
-        roasts = [];
 
         request( this.url, function (error, response, html) {
 
@@ -34,7 +40,7 @@ var scraper = {
 
                 var $ = cheerio.load( html );
                 var roastPages = [];
-
+                //Grab the roastMe pages and push them in an array
                 $( '.comments' ).filter( function () {
 
                     // Let's store the data we filter into a variable so we can easily see what's going on.
@@ -54,35 +60,50 @@ var scraper = {
 
                     if ( !error ) {
                         // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
                         $ = cheerio.load( html );
 
                         $( '.md' ).filter( function () {
-                            data = $( this );
-                            stuff = data.children();
-                            //console.log(stuff);
-                            if ( data.children()[0].name == "p" ) {
-                                var roast = stuff[0].children[0].data;
-                                roasts.push( roast );
-                                //console.log(roast);
+                            var data = $( this );
+                            var votes = data.parent().parent().parent().children().first().children()[5];
+                            if(typeof votes !== 'undefined'){
+                                if(votes.attribs.class == "score likes"){
+                                    votes = votes.children[0].data;
+                                    stuff = data.children();
+                                    var roast = "";
+                                    if ( data.children()[0].name == "p" ) {
+                                        roast = stuff[0].children[0].data;
+                                        //roasts.push( roast );
+                                        that.roasts.push(new Roast(roast,parseInt(votes.split(" ")[0])));
+                                    }
+                                    //console.log("roast: " , roast, " votes: ", votes);
+                                }
                             }
                         } );
-                        fs.writeFile(
-                            "data.txt",
-                            JSON.stringify(roasts),
-                            function (err) {
-                                if (err) {
-                                    console.error('Crap happens');
-                                }
-                        });
-                        console.log( "counter: ", that.counter);
-                        callback(roasts[that.counter]);
-                        that.counter++;
+                        that.roasts = _.sortBy(that.roasts, 'upvotes' ).reverse();
+                        that.roasts = _.filter(that.roasts, function(num){ return num.upvotes >= 4; });
+                        //console.log(that.roasts);
+                        //var msg = that.roasts[that.counter];
+                        //that.counter = that.counter++;
+                        callback("Bot has started");
                     }
                 } );
+                //Write to data.txt
+                fs.writeFile(
+                     "data.txt",
+                     JSON.stringify(that.roasts),
 
+                    function (err) {
+                        if (err) {
+                            console.error('Crap happens');
+                        }
+                });
             }
         } );
+    },
+    getRoast: function(callback){
+        var msg = this.roasts[this.counter];
+        this.counter++;
+        callback(msg.msg);
     }
 
 
